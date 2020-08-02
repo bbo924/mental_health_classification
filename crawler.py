8e486c7f3e7ead47ce22ef0c9770eae379e9c76c
bbo <bobing0924@outlook.com>
2020-08-03 00:12:05 +1000

    Update crawler.py
    one thread crawler with beautifulsoup Done

--- crawler.py
+++ crawler.py
@@ -1,34 +1,5 @@
 from bs4 import BeautifulSoup
 import re
-# 1. Using selenium
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# import time
-
-# browser = webdriver.Chrome(ChromeDriverManager().install())
-# browser.get('https://www.dailystrength.org/group/depression')
-# browser.implicitly_wait(3)
-# time.sleep(3)
-# browser.find_element_by_xpath('//*[@id="load-more-discussions"]').click()
-# i = 0
-# for i in range(2):##这里设置点击5次“加载更多”
-#     print(i)
-#     browser.find_element_by_xpath('//*[@id="load-more-discussions"]').click()
-#     time.sleep(5)###如果网页没有完全加载，会出现点击错误，会点击到某个电影页面，所以加了一个睡眠时间。
-#     ##browswe.page_source是点击5次后的源码，用Beautiful Soup解析源码
-#     soup = BeautifulSoup(browser.page_source, 'html.parser')
-
-# # //*[@id="main-content"]/section/div[3]/div[1]/div/div[2]/ul/li[1]
-# # //*[@id="ds-3816235"]
-# for link in soup.find_all('a'):
-#     print(link.get('href'))
-# # items = soup.find('div', class_=re.compile('list-wp'))
-# # for item in items.find_all('a'):
-# #     Title = item.find('span', class_='title').text
-# #     Rate = item.find('span', class_='rate').text
-# #     Link = item.find('span',class_='pic').find('img').get('src')
-# #     print(Title,Rate,Link)
-
 import requests
 from requests.exceptions import RequestException
 import time
@@ -38,7 +9,7 @@ import datetime
 import sys
 import time
 
-# Method 2: Parsing url link
+# Method 1: Parsing url link (dailyStrength.org)
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 def get_one_page(url):
     try:
@@ -107,3 +78,33 @@ def main():
             
 if __name__=='__main__':
     main()
+
+
+# 2. Using selenium
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
+
+# browser = webdriver.Chrome(ChromeDriverManager().install())
+# browser.get('https://www.dailystrength.org/group/depression')
+# browser.implicitly_wait(3)
+# time.sleep(3)
+# browser.find_element_by_xpath('//*[@id="load-more-discussions"]').click()
+# i = 0
+# for i in range(2):##这里设置点击5次“加载更多”
+#     print(i)
+#     browser.find_element_by_xpath('//*[@id="load-more-discussions"]').click()
+#     time.sleep(5)###如果网页没有完全加载，会出现点击错误，会点击到某个电影页面，所以加了一个睡眠时间。
+#     ##browswe.page_source是点击5次后的源码，用Beautiful Soup解析源码
+#     soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+# # //*[@id="main-content"]/section/div[3]/div[1]/div/div[2]/ul/li[1]
+# # //*[@id="ds-3816235"]
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
+# # items = soup.find('div', class_=re.compile('list-wp'))
+# # for item in items.find_all('a'):
+# #     Title = item.find('span', class_='title').text
+# #     Rate = item.find('span', class_='rate').text
+# #     Link = item.find('span',class_='pic').find('img').get('src')
+# #     print(Title,Rate,Link)
